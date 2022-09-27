a7fd3d7f64d94fb365267666537b63fcf9908b14
shraddhapandeyy <114509913+shraddhapandeyy@users.noreply.github.com>
2022-09-27 22:23:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Home Page')
+})
+
+app.listen(8080, () => {
+    console.log(`server running on port ${PORT}`);
+})
