da846e903b532d2846628edeb5958d908a865796
raji2004 <kingraj1344@gmail.com>
2022-10-01 17:28:20 +0100

    bugs-fixed

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
+// import facultyRoutes from "./routes/faculty.routes.js";
+// ;import libraryRoutes from "./routes/library.routes.js";
+const facultyRoutes = require("./routes/faculty.routes.js");
+const libraryRoutes = require("./routes/library.routes.js");
+const express = require("express");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
 const app = express();
-import facultyRoutes from "./routes/faculty.routes.js";
-import libraryRoutes from "./routes/library.routes.js";
 
 const PORT = process.env.PORT || 8080;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
 
-app.get('/', (req, res) => {
-    res.send('Home Page')
-})
-
-app.use('/faculty', facultyRoutes);
-app.use('/library', libraryRoutes);
+app.use("/faculty", facultyRoutes);
+app.use("/library", libraryRoutes);
 
 app.listen(8080, () => {
-    console.log(`server running on port ${PORT}`);
-})
+  console.log(`server running on port ${PORT}`);
+});
--- routes/library.routes.js
+++ routes/library.routes.js
@@ -1,16 +1,23 @@
-import express from 'express';
-import { addLibrary, deleteLibrary, getLibrary, getLibrarys, updateLibrary } from '../controllers/Librarys.js';
+// import express from 'express';
+// import { addLibrary, deleteLibrary, getLibrary, getLibrarys, updateLibrary } from '../controllers/Librarys.js';
+const express = require("express");
+const {
+  addLibrary,
+  deleteLibrary,
+  getLibrary,
+  getLibrarys,
+  updateLibrary,
+} = require("../controllers/Librarys.js");
 
 const router = express.Router();
 
 // Do crud operations here
-let Librarys = []
+let Librarys = [];
 
-router.get('/', getLibrarys)
-router.get('/:id', getLibrary)
-router.post('/', addLibrary)
-router.delete('/:id', deleteLibrary)
-router.patch('/:id', updateLibrary)
+router.get("/", getLibrarys);
+router.get("/:id", getLibrary);
+router.post("/", addLibrary);
+router.delete("/:id", deleteLibrary);
+router.patch("/:id", updateLibrary);
 
-
-export default router;
+module.exports = router;
