b19418dd90009a7fed348aec8aca940bc240c4db
Abhishek Singh <abhisheksingh9793510@gmail.com>
2022-10-02 16:39:17 +0530

    libraryRoutes created

---
+++ routes/libraryRoutes.js
@@ -0,0 +1,24 @@
+// library routes
+// import express from 'express';
+// import { addLibrary, deleteLibrary, getLibrary, getLibrarys, updateLibrary } from '../controllers/Librarys.js';
+const express = require("express");
+const {
+  addLibrary,
+  deleteLibrary,
+  getLibrary,
+  getLibrarys,
+  updateLibrary,
+} = require("../controllers/Librarys.js");
+
+const router = express.Router();
+
+// Do crud operations here
+let Librarys = [];
+
+router.get("/", getLibrarys);
+router.get("/:id", getLibrary);
+router.post("/", addLibrary);
+router.delete("/:id", deleteLibrary);
+router.patch("/:id", updateLibrary);
+
+module.exports = router;
