0f3581ced63bd8455c22ae4dc57c911a9ec956e6
Abhishek Singh <abhisheksingh9793510@gmail.com>
2022-10-04 09:40:22 +0530

    student middleware completed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 // import facultyRoutes from "./routes/faculty.routes.js";
 // ;import libraryRoutes from "./routes/library.routes.js";
-const facultyRoutes = require("./routes/faculty.routes.js");
-const libraryRoutes = require("./routes/library.routes.js");
+const facultyRoutes = require("./routes/facultyRoutes.js");
+const libraryRoutes = require("./routes/libraryRoutes.js");
+const studentRoutes = require("./routes/studentRoutes.js");
 const express = require("express");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const app = express();
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'sms'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
+
 
 const PORT = process.env.PORT || 8080;
 
@@ -16,6 +34,7 @@ app.get("/", (req, res) => {
   res.send("Home Page");
 });
 
+app.use("/student", studentRoutes);
 app.use("/faculty", facultyRoutes);
 app.use("/library", libraryRoutes);
 
--- middlewares/studentAuth.js
+++ middlewares/studentAuth.js
@@ -1,1 +1,21 @@
 // student authentication middleware
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const isStudentLogedIn = (req, res, next)=>{
+    //Get the user from the jwt token and id to req object
+    const token = req.header("student-auth-token");
+    if(!token){
+        res.status(401).send({error:"Somthing went wrong please login again"});
+    }
+    try {
+        const data = jwt.verify(token, JWT_SECRET);
+        req.user = data.user;
+        next();
+    } catch (error) {
+        res.status(401).send({error:"Somthing went wrong please login again"});
+    }
+    
+}
+
+module.exports = isStudentLogedIn;
