7dbd286732866a42eaaf03b00350828f2e3c8cfd
Sukriti <sukritimaurya.fzd@gmail.com>
2022-10-10 00:45:33 +0530

    add classes

--- models/classesModel.js
+++ models/classesModel.js
@@ -1,1 +1,37 @@
 // classes model
+
+const mongoose = require("mongoose");
+
+
+const ClassSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+room:{type:String,
+required:true},
+    mentors: [
+      {
+        type: String,
+      }
+    ],
+    date:
+        {
+          type: Date,
+          required:true
+
+        }
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+const Class = mongoose.model("Class", ClassSchema);
+module.exports = Class; 
--- routes/classesRoutes.js
+++ routes/classesRoutes.js
@@ -1,1 +1,20 @@
 // classes routes
+
+const express = require("express");
+const {
+  getClasses,
+  getClass,
+  addClass,
+  deleteClass,
+  updateClass,
+} = require("../controllers/Classes.js");
+
+const router = new express.Router();
+
+router.get("/",  getClasses);
+router.get("/:id", getClass);
+router.post("/",  addClass);
+router.delete("/:id", deleteClass);
+router.patch("/:id",   updateClass);
+
+module.exports = router;
